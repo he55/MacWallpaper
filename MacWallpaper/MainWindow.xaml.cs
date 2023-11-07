682702deb7a367c2861a695e4c8297b568f77040
he55 <40764272+he55@users.noreply.github.com>
2023-11-07 23:15:35 +0800

    load image

--- MacWallpaper/MainWindow.xaml.cs
+++ MacWallpaper/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,9 +27,29 @@ namespace MacWallpaper
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            WebClient webClient = new WebClient();
+            string imgsPath = "images";
+            if(!Directory.Exists(imgsPath))
+            {
+                Directory.CreateDirectory(imgsPath);
+            }
+
             var model= ModelHelper.Load();
+            Asset[] assets = model.categories[0].assets;
+            foreach ( Asset asset in assets )
+            {
+                string v = new Uri(asset.previewImage).Segments.Last();
+                string v1 = System.IO.Path.Combine(imgsPath, v);
+                string v2 = System.IO.Path.GetFullPath(v1);
+                if(!File.Exists(v2))
+                {
+                   await webClient.DownloadFileTaskAsync(asset.previewImage, v2);
+                }
+                asset.previewImage = v2;
+            }
+            gridView.ItemsSource = assets;
         }
     }
 }
