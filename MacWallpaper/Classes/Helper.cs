c06cedafcfe125766a610ad93adfb34a5e40c91b
he55 <40764272+he55@users.noreply.github.com>
2023-11-22 19:51:08 +0800

    add class

---
+++ MacWallpaper/Classes/Helper.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DreamScene2
+{
+    public static class Helper
+    {
+        const string STARTUP_KEY = @"Software\Microsoft\Windows\CurrentVersion\Run";
+        static string s_appPath;
+
+        public static void OpenLink(string link)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = link,
+                UseShellExecute = true
+            });
+        }
+
+        public static string GetPathForStartupFolder(string subPath)
+        {
+            return Path.Combine(Application.StartupPath, subPath);
+        }
+
+        public static string GetPathForUserAppDataFolder(string subPath)
+        {
+            if (s_appPath == null)
+            {
+                s_appPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Constant.ProjectName);
+                if (!Directory.Exists(s_appPath))
+                {
+                    Directory.CreateDirectory(s_appPath);
+                }
+            }
+            return Path.Combine(s_appPath, subPath);
+        }
+
+        public static bool CheckStartOnBoot()
+        {
+            RegistryKey startupKey = Registry.CurrentUser.OpenSubKey(STARTUP_KEY);
+            bool startOnBoot = startupKey.GetValue(Constant.ProjectName) != null;
+            startupKey.Close();
+            return startOnBoot;
+        }
+
+        public static void SetStartOnBoot()
+        {
+            RegistryKey startupKey = Registry.CurrentUser.OpenSubKey(STARTUP_KEY, true);
+            startupKey.SetValue(Constant.ProjectName, $"\"{Application.ExecutablePath}\" -b");
+            startupKey.Close();
+        }
+
+        public static void RemoveStartOnBoot()
+        {
+            RegistryKey startupKey = Registry.CurrentUser.OpenSubKey(STARTUP_KEY, true);
+            startupKey.DeleteValue(Constant.ProjectName);
+            startupKey.Close();
+        }
+    }
+}
---
+++ MacWallpaper/Classes/Settings.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace DreamScene2
+{
+    public class Settings
+    {
+        static string s_filePath = Helper.GetPathForUserAppDataFolder("settings.xml");
+        static Settings s_settings;
+
+        private Settings() { }
+
+        public bool FirstRun { get; set; } = true;
+        public bool AutoPlay { get; set; } = true;
+        public int PlayMode { get; set; }
+
+        public bool AutoPause1 { get; set; }
+        public bool AutoPause2 { get; set; }
+        public bool AutoPause3 { get; set; }
+
+        public bool IsMuted { get; set; }
+        public bool DisableWebSecurity { get; set; } = true;
+        public bool UseDesktopInteraction { get; set; } = true;
+
+        public bool CanPause()
+        {
+            return AutoPause1 || AutoPause2 || AutoPause3;
+        }
+
+        public static Settings Load()
+        {
+            if (s_settings == null)
+            {
+                if (File.Exists(s_filePath))
+                {
+                    using (FileStream fileStream = File.OpenRead(s_filePath))
+                    {
+                        XmlSerializer xmlSerializer = new XmlSerializer(typeof(Settings));
+                        s_settings = (Settings)xmlSerializer.Deserialize(fileStream);
+                    }
+                }
+                else
+                {
+                    s_settings = new Settings();
+                }
+            }
+            return s_settings;
+        }
+
+        public static void Save()
+        {
+            using (FileStream fileStream = File.Create(s_filePath))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Settings));
+                xmlSerializer.Serialize(fileStream, s_settings);
+            }
+        }
+    }
+}
