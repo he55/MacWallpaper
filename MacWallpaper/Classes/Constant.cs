a841d9bb319191ff8c57f1be4914433874c1fb5b
he55 <40764272+he55@users.noreply.github.com>
2023-11-22 23:28:53 +0800

    Update Constant.cs

--- MacWallpaper/Classes/Constant.cs
+++ MacWallpaper/Classes/Constant.cs
@@ -2,7 +2,7 @@
 {
     public static class Constant
     {
-        public const string ProjectName = "DreamScene2";
+        public const string ProjectName = "4kwallpaper";
         public const string Version = "1.6";
         public const string MainWindowTitle = ProjectName + " (～￣▽￣)～";
     }
