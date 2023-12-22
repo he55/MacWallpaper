d464573a78b525fe714082af8472d5ac67e7b253
he55 <40764272+he55@users.noreply.github.com>
2023-12-22 22:02:13 +0800

    Update DownloadStateToVisibilityConverter.cs

--- MacWallpaper/DownloadStateToVisibilityConverter.cs
+++ MacWallpaper/DownloadStateToVisibilityConverter.cs
@@ -10,11 +10,7 @@ namespace MacWallpaper
         public DownloadState State { get; set; }
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if(value is DownloadState downloadState)
-            {
-                return State==downloadState?Visibility.Visible:Visibility.Collapsed;
-            }
-            throw new NotImplementedException();
+            return State == ((DownloadState)value) ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
