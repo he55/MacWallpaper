f84b854b8c69d9d96ee0a17d921c4fe79ce98bad
he55 <40764272+he55@users.noreply.github.com>
2023-12-28 23:31:39 +0800

    4

---
+++ MacWallpaper/Constant.cs
@@ -0,0 +1,9 @@
+﻿namespace DreamScene2
+{
+    public static class Constant
+    {
+        public const string ProjectName = "DreamScene2";
+        public const string Version = "1.7";
+        public const string MainWindowTitle = ProjectName + " (～￣▽￣)～";
+    }
+}
